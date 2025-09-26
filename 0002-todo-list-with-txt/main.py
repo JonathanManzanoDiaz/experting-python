7975893f048c7ef96debc1c2311642d0be5fbaaf
Jonathan Manzano Diaz <jonathanmanzanodiaz@gmail.com>
2025-09-26 19:51:30 +0200

    Upgrading project 0002

--- 0002-todo-list-with-txt/main.py
+++ 0002-todo-list-with-txt/main.py
@@ -1,87 +1,61 @@
+def show_options():
+    print('\nWelcome to To Do List with File txt')
+    print('1 - Add task')
+    print('2 - Delete task')
+    print('3 - Show all tasks')
+    print('0 - Exit')
 
-# open the file as read and write
-tasks_file = open("tasks.txt", "a+")
+def add_tasks(task):
+    with open("tasks.txt", 'a') as f:
+        f.write(task)
+        print(f"The task is added succesfully!")
 
-def read_tasks(f):
-  #READ ALL THE LINES AGAIN
-  f.seek(0)
-  return f.readlines()
-
-def write_tasks(f, lines):
-  #WRITE ALL THE TASKS AGAIN
-  norm = [(ln.rstrip("\n") + "\n") for ln in lines]
-  f.seek(0)
-  f.truncate(0)
-  f.writelines(norm)
-  f.flush()
-
-def ask_int(prompt):
-  val = input(prompt).strip()
-  return int(val) if val.isdigit() else None
-
-# Option input
-option = -1
-# Option loop
-while option != 0:
-  tasks_file.seek(0)
-
-  #MENU TASKS
-  title = "Menu".upper()
-  print(title.center(20, "="))
-  print("Tasks".center(20))
-  
-  # READ LINES of the file
-  content = read_tasks(tasks_file)
-  
-  # PRINT THE TASKS, IF It's empty, Tell there is no task
-  if len(content) == 0:
-    print("You don't have any tasks yet")
-  else:
-    for index, task in enumerate(content, start=1):
-      print(index, task.strip())
-  
-  # print "---" for separate the options from the tasks
-  print("-".center(20, "-"))
-  
-  # Print the options
-  print("Add".ljust(16, ".") + "1".rjust(4))
-  print("Delete".ljust(16, ".") + "2".rjust(4))
-  print("Update Task".ljust(16, ".") + "3".rjust(4))
-  print("Exit".ljust(16, ".") + "0".rjust(4))
-  
-  
-  # Select the option
-  option = int(input("Enter your option: "))
-  if option == 1:
-    # ADD THE TASK
-    task = input("Enter the task you want to add: ")
-    tasks_file.write(task + "\n")
-    tasks_file.flush()
-  
-  elif option == 2:
-    # DELETE THE TASK
-    if not content:
-      print("There are no tasks to delete.")
-      continue
-    n = ask_int("Enter the number of the task you want to delete: ")
-    if n is None or n < 1 or n > len(content):
-      print("This number is out of range")
-      continue
-    content.pop(n - 1)
-    write_tasks(tasks_file, content)
-    
-  elif option == 3:
-    # UPDATE THE TASK
-    idx = ask_int("Enter the number of the task you want to update: ") - 1
-    
-    if idx < 0 or idx >= len(content):
-      print("This number is out of range")
+def delete_task():
+    tasks = []
+    with open("tasks.txt", "r", encoding="utf8") as f:
+        f.seek(0)
+        lines = f.readlines()
+        for line in lines:
+            tasks.append(line)
+        f.close()
+    if len(tasks) > 0:
+        show_tasks()
     else:
-      new_task = input("Enter the update of the task: ").strip()
-      content[idx] = new_task.rstrip("\n") + "\n"
-      write_tasks(tasks_file, content)
-  elif option == 0:
-    # EXIT THE APP
-   print("\n Goodbye. Have a nice day!")
+        print("There is not tasks in the list.")
+        
+    task = int(input('What task do you want to delete?: '))
+    try:
+        if len(tasks) >= task:
+            tasks.pop(task - 1)
+        else:
+            print("Error, this task number doesn't exist")
+    except ValueError:
+        print('The list is empty!')
+    with open('tasks.txt', 'w', encoding='utf8') as f:
+        f.seek(0)
+        for task in tasks:
+            f.write(task)
 
-tasks_file.close()
+def show_tasks():
+    with open("tasks.txt", "r", encoding="utf8") as f:
+        lines = f.readlines()
+        if len(lines) > 0:
+            for i, line in enumerate(lines, start=1):
+                print(i, line.strip('\n'))
+            f.close()
+        else:
+            print("There is not tasks in the list.")
+
+while True:
+    show_options()
+    option = int(input('Select an option: '))
+    if option == 1:
+        task = input('What task do you want to add?: ')
+        add_tasks(f"{task}\n" )
+    elif option == 2:
+        delete_task()
+    elif option == 3:
+        show_tasks()
+    else:
+        print('Goodbye, Have a nice day!')
+        break
