d7ff6a9bfcadbb7e8f23ebaa6756152d43a4063b
Jonathan Manzano Diaz <jonathanmanzanodiaz@gmail.com>
2025-09-30 19:16:26 +0200

    Uploading past project

--- 0030-django-gestor-notes/gestor_notes/notes/urls.py
+++ 0030-django-gestor-notes/gestor_notes/notes/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'notes'
+
 urlpatterns = [
-    path('', views.index, name='index'),    
-    path("notes/create/", views.create_note, name="create_note"),
-    path("notes/update/", views.update_note, name="update_note"),
+    path('', views.index, name='index'),
+    path('create_note/', views.create_note, name='create_note'),
+    path('note/<int:pk>/', views.note_details, name='note_details'),
+    path("notes/<int:pk>/edit/", views.edit_note, name="edit_note")
 
 ]
--- 0030-django-gestor-notes/gestor_notes/notes/views.py
+++ 0030-django-gestor-notes/gestor_notes/notes/views.py
@@ -1,29 +1,49 @@
+from datetime import timezone
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Note
-
+# Create your views here.
 
 def index(request):
-    notes = Note.objects.all().order_by("-updated_at")
-    return render(request, "core/index.html", {"notes": notes})
+    notes = Note.objects.all().order_by('-updated_at')
+    return render(request, 'notes/index.html', {
+        'notes': notes,
+    })
 
-def update_note(request):
-    if request.method == "POST":
-        note_id = request.POST.get("note_id")
-        note = get_object_or_404(Note, id=note_id)
-        note.title = request.POST.get("title")
-        note.content = request.POST.get("content")
-        note.save()
-        return redirect("index")
-    
 
 def create_note(request):
+    if request.method == 'POST':
+        title = request.note.get('title')
+        content = request.note.get('content')
+        if title and content:
+            Note.objects.create(title=title, content=content)
+            return redirect('notes:index')
+        else:
+            return redirect('notes:create_note')
+    return render(request, 'notes/create_note.html')
+
+
+def note_details(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+    return render(request, 'notes/note_details.html', {
+        'note': note,
+    })
+
+def edit_note(request, pk):
+    note = get_object_or_404(Note, pk=pk)
+
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
 
         if title and content:
-            Note.objects.create(title=title, content=content)
+            note.title = title
+            note.content = content
+            note.save()
+            return redirect("notes:note_details", pk=note.pk)
 
-        return redirect("index")
+    return render(request, "notes/note_edit.html", {
+        "note": note,
+    })
 
-    return redirect("index")
+
+
